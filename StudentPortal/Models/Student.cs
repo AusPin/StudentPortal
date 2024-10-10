45130faf2f5fc9209365027f64d49575e50c48a5
AusPin <103032413+AusPin@users.noreply.github.com>
2024-10-10 22:34:15 +0800

    Added Student class

---
+++ StudentPortal/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentPortal.Models
+{
+    public class Student
+    {
+        public int studentId {  get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public List<Enrollment> Enrollments { get; set; }
+    }
+}
